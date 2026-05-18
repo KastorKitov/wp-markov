28f830565343445a97c14b826cc726f619e22091
Kastor <kitov.kastor@gmail.com>
2026-05-18 12:48:27 +0300

    href updated

--- wp-content/plugins/kastor-machines/single-machine.php
+++ wp-content/plugins/kastor-machines/single-machine.php
@@ -166,9 +166,16 @@ get_header();
 							</section>
 						<?php endif; ?>
 
-						<a class="kastor-machine__cta" href="<?php echo esc_url( kastor_machines_get_inquiry_url() ); ?>">
-							Свържете се с нас
-						</a>
+						<div class="kastor-machine__ctas">
+							<a class="kastor-machine__cta" href="<?php echo esc_url( home_url( '/контакти/' ) ); ?>">
+								Свържете се с нас
+							</a>
+							<a class="kastor-machine__cta-phone" href="tel:+359879156316" aria-label="Обади се на +359 879 156 316" title="+359 879 156 316">
+								<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+									<path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+								</svg>
+							</a>
+						</div>
 
 					</div><!-- /.kastor-machine__body -->
 				<?php endif; ?>
